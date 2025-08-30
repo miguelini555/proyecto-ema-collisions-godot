9840c54632f0c374ca3e2a18a9863541d38e9588
miguelini555 <guevarasarmientoemanuel4@gmail.com>
2025-08-30 14:49:23 -0400

    Cuarto commit

--- scripts/door.gd
+++ scripts/door.gd
@@ -7,9 +7,9 @@ func _ready() -> void:
 	close()
 
 func open():
-	$CollisionShape2D.disabled = true
+	$CollisionShape2D.disabled = false
 	$Sprite2D.texture = open_texture
 	
 func close():
-	$CollisionShape2D.disabled = false
+	$CollisionShape2D.disabled = true
 	$Sprite2D.texture = closed_texture
