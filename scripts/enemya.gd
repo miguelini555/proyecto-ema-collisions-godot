d721c16b2c48f7373327c8741c6ed8121247c854
miguelini555 <guevarasarmientoemanuel4@gmail.com>
2025-09-01 11:02:34 -0400

    Octavo commit

--- scripts/enemya.gd
+++ scripts/enemya.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 
 func _on_activate_zone_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	pass
 
 
 func _on_activate_zone_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	pass
